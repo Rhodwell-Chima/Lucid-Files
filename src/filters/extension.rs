1700edede3f31b74a0a44e4b4a7a49a98ff0404c
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-11-12 15:00:15 +0200

    Add `ExtensionFilter` implementation for filtering files by extension

---
+++ src/filters/extension.rs
@@ -0,0 +1,27 @@
+use super::{FileFilter, FilterError};
+use std::ffi::OsStr;
+use std::path::Path;
+pub struct ExtensionFilter {
+    allowed: Vec<String>,
+}
+
+impl ExtensionFilter {
+    fn new<S: AsRef<str>>(extensions: Vec<S>) -> Self {
+        Self {
+            allowed: extensions
+                .into_iter()
+                .map(|s| s.as_ref().to_lowercase())
+                .collect(),
+        }
+    }
+}
+
+impl FileFilter for ExtensionFilter {
+    fn matches(&self, path: &Path) -> Result<bool, FilterError> {
+        let result = path
+            .extension()
+            .and_then(OsStr::to_str)
+            .map(|extension| self.allowed.contains(&extension.to_lowercase()));
+        Ok(result.unwrap_or_else(|| false))
+    }
+}
