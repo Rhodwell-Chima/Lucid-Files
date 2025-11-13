3b8737510dc0e842696e6b3c715603c5b6008aae
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-11-13 16:23:36 +0200

    Update `Scanner` trait to return `Result` with `FilterError`

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -1,7 +1,9 @@
 pub mod simple;
 
+use std::io::Error;
 use std::path::{Path, PathBuf};
+use crate::filters::FilterError;
 
 pub trait Scanner {
-    fn scan(&self, root: &Path) -> Vec<PathBuf>;
+    fn scan(&self, root: &Path) -> Result<Vec<PathBuf>, FilterError>;
 }
