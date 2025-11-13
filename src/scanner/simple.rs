7ed0a938cd3588bc3ae35f2c457ca2d2fe3a86d8
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-11-13 16:23:04 +0200

    Add `SimpleScanner` implementation with `MultiFilter` support

---
+++ src/scanner/simple.rs
@@ -0,0 +1,28 @@
+use crate::filters::filter_chain::MultiFilter;
+use crate::filters::{FileFilter, FilterError};
+use crate::scanner::Scanner;
+use std::fs;
+use std::path::{Path, PathBuf};
+
+pub struct SimpleScanner {
+    filter: MultiFilter,
+}
+
+impl SimpleScanner {
+    pub fn new(filter: MultiFilter) -> Self {
+        Self { filter }
+    }
+}
+
+impl Scanner for SimpleScanner {
+    fn scan(&self, root: &Path) -> Result<Vec<PathBuf>, FilterError> {
+        let mut paths = vec![];
+        for entry in fs::read_dir(root).map_err(FilterError::IoError)? {
+            let file_path = entry.map_err(FilterError::IoError)?.path();
+            if self.filter.matches(&file_path)? {
+                paths.push(file_path);
+            }
+        }
+        Ok(paths)
+    }
+}
