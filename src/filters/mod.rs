e3adee4e5a187ecec21f5c2961c781ddc800dda5
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-11-13 15:31:42 +0200

    Make filter modules public to allow external usage

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -2,14 +2,14 @@ use std::io::Error;
 use std::path::Path;
 use std::time::SystemTimeError;
 
-mod date;
-mod extension;
-mod file_type;
-mod name;
-mod owner;
-mod permissions;
-mod size;
-mod filter_chain;
+pub mod date;
+pub mod extension;
+pub mod file_type;
+pub mod filter_chain;
+pub mod name;
+pub mod owner;
+pub mod permissions;
+pub mod size;
 
 pub trait FileFilter {
     fn matches(&self, path: &Path) -> Result<bool, FilterError>;
