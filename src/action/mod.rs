f0febe82a40c2e50341c8a81c6e11f2c49634479
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-11-18 16:39:26 +0200

    Add FileAction trait

--- src/action/mod.rs
+++ src/action/mod.rs
@@ -1,3 +1,14 @@
-mod copy;
-mod move_file;
-mod delete;
+use std::io::Error;
+use std::path::Path;
+
+pub mod copy;
+pub mod delete;
+pub mod move_file;
+
+pub use copy::*;
+pub use delete::*;
+pub use move_file::*;
+
+pub trait FileAction {
+    fn execute(&self) -> Result<(), Error>;
+}
