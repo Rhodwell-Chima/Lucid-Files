536bf59b074df40119690ed5b53aa33dd1398b7f
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-11-19 10:35:09 +0200

    Add secure_move module

--- src/action/mod.rs
+++ src/action/mod.rs
@@ -4,10 +4,12 @@ use std::path::Path;
 pub mod copy;
 pub mod delete;
 pub mod move_file;
+pub mod secure_move;
 
 pub use copy::*;
 pub use delete::*;
 pub use move_file::*;
+pub use secure_move::*;
 
 pub trait FileAction {
     fn execute(&self) -> Result<(), Error>;
