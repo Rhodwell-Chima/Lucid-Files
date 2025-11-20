e402c3ffd910e5ab8b7f45cb171e76c66c431e83
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-11-20 13:07:20 +0200

    Add `ClassifierError` enum for error handling in `FileClassifier`

--- src/classifier/mod.rs
+++ src/classifier/mod.rs
@@ -1,13 +1,39 @@
-pub mod extension;
 mod date;
-mod size;
+pub mod extension;
 mod name;
+mod size;
 
 pub use extension::*;
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 
-use std::io::Error;
 use std::path::Path;
 
 pub trait FileClassifier {
-    fn classify(&self, path: &Path) -> Result<String, Error>;
+    fn classify(&self, path: &Path) -> Result<Option<String>, ClassifierError>;
 }
+
+#[derive(Debug)]
+pub enum ClassifierError {
+    InvalidFileName,
+    MissingExtension,
+    InvalidExtensionEncoding,
+}
+
+impl Display for ClassifierError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ClassifierError::InvalidFileName => {
+                write!(f, "filename is not valid UTF-8")
+            }
+            ClassifierError::MissingExtension => {
+                write!(f, "file has no extension")
+            }
+            ClassifierError::InvalidExtensionEncoding => {
+                write!(f, "extension is not valid UTF-8")
+            }
+        }
+    }
+}
+
+impl Error for ClassifierError {}
