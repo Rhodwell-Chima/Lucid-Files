ad57d76fc11613177d48e26ce6b3248280c22952
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-11-21 17:25:43 +0200

    Refactor: Use `FileActionError` in `SecureMoveAction` for consistent error handling

--- src/action/secure_move.rs
+++ src/action/secure_move.rs
@@ -1,4 +1,4 @@
-use crate::action::FileAction;
+use crate::action::{FileAction, FileActionError};
 use std::fs;
 use std::io::Error;
 use std::path::PathBuf;
@@ -17,9 +17,9 @@ impl SecureMoveAction {
 }
 
 impl FileAction for SecureMoveAction {
-    fn execute(&self) -> Result<(), Error> {
-        fs::copy(&self.source, &self.destination)?;
-        fs::remove_file(&self.source)?;
+    fn execute(&self) -> Result<(), FileActionError> {
+        fs::copy(&self.source, &self.destination).map_err(|e| FileActionError::Io(e))?;
+        fs::remove_file(&self.source).map_err(|e| FileActionError::Io(e))?;
         Ok(())
     }
 }
