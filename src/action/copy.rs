73217aa89b8408255b4e0b1ec41dc4deab66c6c3
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-11-22 15:12:23 +0200

    Refactor: Add `CopyActionRef` with borrowed paths for efficient file operations

--- src/action/copy.rs
+++ src/action/copy.rs
@@ -1,7 +1,7 @@
 use crate::action::{FileAction, FileActionError};
 use std::fs;
 use std::io::Error;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 pub struct CopyAction {
     source: PathBuf,
@@ -20,6 +20,27 @@ impl CopyAction {
 impl FileAction for CopyAction {
     fn execute(&self) -> Result<(), FileActionError> {
         fs::copy(&self.source, &self.destination).map_err(|e| FileActionError::Io(e))?;
+        Ok(())
+    }
+}
+
+pub struct CopyActionRef<'a> {
+    source: &'a Path,
+    destination: &'a Path,
+}
+
+impl<'a> CopyActionRef<'a> {
+    pub fn new(source: &'a Path, destination: &'a Path) -> Self {
+        Self {
+            source,
+            destination,
+        }
+    }
+}
+
+impl<'a> FileAction for CopyActionRef<'a> {
+    fn execute(&self) -> Result<(), FileActionError> {
+        fs::copy(self.source, self.destination).map_err(FileActionError::Io)?;
         Ok(())
     }
 }
