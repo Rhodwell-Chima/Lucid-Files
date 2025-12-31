e8810a99e0e06ee25d3abbc85ebba00551b4c327
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-12-31 19:43:38 +0200

    Simplify `mod.rs` by re-exporting `config` module with `pub use`.

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,9 +1,9 @@
 use std::fs;
 use std::io::{Error, ErrorKind};
 use std::path::Path;
-use crate::config::config::Config;
 
 pub mod config;
+pub use config::*;
 
 pub fn load_config_from_path(config_dir: &Path) -> Result<Config, Error> {
     let content = fs::read_to_string(config_dir)?;
