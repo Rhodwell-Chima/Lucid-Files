014eaed45f3d2f3373d57bfef4bcc87df9afe496
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2026-01-07 20:49:03 +0200

    Add tests for CopyAction and CopyActionRef to validate copy functionality

--- src/action/copy.rs
+++ src/action/copy.rs
@@ -47,13 +47,59 @@ impl<'a> FileAction for CopyActionRef<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Write;
+    use tempfile::NamedTempFile;
     #[test]
-    fn copy_ref_test_normal() {
+    fn copy_ref_fail_test() {
         let copy_struct = CopyActionRef {
             source: Path::new(""),
             destination: Path::new(""),
         };
 
         assert!(copy_struct.execute().is_err())
+    }
+
+    #[test]
+    fn copy_ref_success_test() {
+        let mut src_file = NamedTempFile::new().unwrap();
+        writeln!(src_file, "Source").unwrap();
+        let src_path = src_file.path();
+
+        let dest_file = NamedTempFile::new().unwrap();
+        let dest_path = dest_file.path().to_path_buf();
+        drop(dest_file);
+
+        let copy_action = CopyActionRef::new(src_path, &dest_path);
+        assert!(copy_action.execute().is_ok());
+
+        let content = fs::read_to_string(&dest_path).unwrap();
+        assert_eq!(content.trim(), "Source");
+    }
+
+    #[test]
+    fn copy_fail_test() {
+        let copy_struct = CopyAction {
+            source: PathBuf::from(""),
+            destination: PathBuf::from(""),
+        };
+
+        assert!(copy_struct.execute().is_err())
+    }
+
+    #[test]
+    fn copy_success_test() {
+        let mut src_file = NamedTempFile::new().unwrap();
+        writeln!(src_file, "Source").unwrap();
+        let src_path = src_file.path().to_path_buf();
+
+        let dest_file = NamedTempFile::new().unwrap();
+        let dest_path = dest_file.path().to_path_buf();
+        drop(dest_file);
+
+        let copy_action = CopyAction::new(src_path, dest_path.clone());
+        assert!(copy_action.execute().is_ok());
+
+        let content = fs::read_to_string(&dest_path).unwrap();
+        assert_eq!(content.trim(), "Source");
     }
 }
